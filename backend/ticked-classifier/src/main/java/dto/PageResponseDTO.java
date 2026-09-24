fd6c6b3893f87ff81920bf7c861d077b7cc30e16
Aleksander <aprokhorenkodev@gmail.com>
2026-09-24 13:18:09 +0200

    feat: create a page response DTO

---
+++ backend/ticked-classifier/src/main/java/dto/PageResponseDTO.java
@@ -0,0 +1,31 @@
+package dto;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import lombok.Data;
+
+@Data
+public class PageResponseDTO<T> {
+
+    private List<T> content;
+    private Integer currentPage;
+    private Integer totalPages;
+    private Long totalElements;
+
+    public PageResponseDTO(Page<T> page) {
+        this.content = page.getContent();
+        this.currentPage = page.getNumber();
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalElements();
+    }
+
+    public PageResponseDTO(List<T> content, Integer currentPage, Integer totalPages, Long totalElements) {
+        this.content = content;
+        this.currentPage = currentPage;
+        this.totalPages = totalPages;
+        this.totalElements = totalElements;
+    }
+
+}
