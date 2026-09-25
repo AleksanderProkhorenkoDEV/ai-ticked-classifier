5cd083f47234854e2e5980c3b4a91a90fa4e8066
Aleksander <aprokhorenkodev@gmail.com>
2026-09-25 11:48:37 +0200

    feat: create new endpoint

--- backend/ticked-classifier/src/main/java/dev/aprokhorenko/ticked_classifier/controllers/TickedController.java
+++ backend/ticked-classifier/src/main/java/dev/aprokhorenko/ticked_classifier/controllers/TickedController.java
@@ -3,15 +3,21 @@ package dev.aprokhorenko.ticked_classifier.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.aprokhorenko.ticked_classifier.dto.CreateTickedRequestDTO;
+import dev.aprokhorenko.ticked_classifier.dto.GlobalResponseDTO;
 import dev.aprokhorenko.ticked_classifier.dto.PageResponseDTO;
 import dev.aprokhorenko.ticked_classifier.dto.TickedResponseDTO;
 import dev.aprokhorenko.ticked_classifier.mappers.TickedMapper;
 import dev.aprokhorenko.ticked_classifier.services.TickedServices;
+import jakarta.validation.Valid;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/tickeds")
@@ -33,6 +39,12 @@ public class TickedController {
         Page<TickedResponseDTO> dtoPage = tickedServices.getAllTickeds(page, size).map(tickedMapper::toDto);
 
         return ResponseEntity.ok(new PageResponseDTO<>(dtoPage));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<GlobalResponseDTO> createTicked(@Valid @RequestBody CreateTickedRequestDTO request) {
+        tickedServices.createTicked(request);
+        return ResponseEntity.ok(new GlobalResponseDTO("Ticked creado", HttpStatus.CREATED.value()));
     }
 
 }
