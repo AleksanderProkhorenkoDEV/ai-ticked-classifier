51d0d1f060ef4aea3f107617797fe88475d050a3
Aleksander <aprokhorenkodev@gmail.com>
2026-09-26 10:55:59 +0200

    refactor: add two trycatch to avoid run time exception and manage the error

--- backend/ticked-classifier/src/main/java/dev/aprokhorenko/ticked_classifier/services/TicketClasificationServices.java
+++ backend/ticked-classifier/src/main/java/dev/aprokhorenko/ticked_classifier/services/TicketClasificationServices.java
@@ -1,15 +1,14 @@
 package dev.aprokhorenko.ticked_classifier.services;
 
-import java.util.List;
-import java.util.Map;
+import java.util.logging.Logger;
 
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.mistralai.MistralAiChatOptions;
 import org.springframework.ai.mistralai.api.MistralAiApi;
 import org.springframework.ai.mistralai.api.MistralAiApi.ChatCompletionRequest.ResponseFormat;
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.stereotype.Service;
 
 import dev.aprokhorenko.ticked_classifier.dto.TicketClasification;
@@ -19,9 +18,13 @@ import tools.jackson.databind.ObjectMapper;
 public class TicketClasificationServices {
 
     private final ChatModel chatModel;
+    private final Logger log;
+    private final ObjectMapper mapper;
 
     public TicketClasificationServices(ChatModel chatModel) {
         this.chatModel = chatModel;
+        this.log = Logger.getLogger("Ticket Clasifier Services");
+        this.mapper = new ObjectMapper();
     }
 
     public TicketClasification classifyTicket(String title, String description) {
@@ -36,10 +39,20 @@ public class TicketClasificationServices {
                 .responseFormat(ResponseFormat.jsonSchema(TicketClasification.class))
                 .build();
 
-        ChatResponse response = chatModel.call(new Prompt(prompt, options));
-        ObjectMapper mapper = new ObjectMapper();
-        TicketClasification result = mapper.readValue(response.getResult().getOutput().getText(),
-                TicketClasification.class);
-        return result;
+        String rawOutput;
+        try {
+            ChatResponse response = chatModel.call(new Prompt(prompt, options));
+            rawOutput = response.getResult().getOutput().getText();
+        } catch (Exception e) {
+            log.severe("Error en la llamada al LLM");
+            throw new RuntimeException("No se pudo obtener respuesta del LLM", e);
+        }
+
+        try {
+            return this.mapper.readValue(rawOutput, TicketClasification.class);
+        } catch (JsonParseException e) {
+            log.severe("Error al convertir el JSON");
+            throw new RuntimeException("El modelo tiene un formato inesperado", e);
+        }
     }
 }
