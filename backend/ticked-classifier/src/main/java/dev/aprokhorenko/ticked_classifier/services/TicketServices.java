0d9105ff8139bc88b731aa3a460154b10bb5953f
Aleksander <aprokhorenkodev@gmail.com>
2026-09-26 10:55:41 +0200

    feat: finished the create ticked method

--- backend/ticked-classifier/src/main/java/dev/aprokhorenko/ticked_classifier/services/TicketServices.java
+++ backend/ticked-classifier/src/main/java/dev/aprokhorenko/ticked_classifier/services/TicketServices.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import dev.aprokhorenko.ticked_classifier.dto.CreateTicketRequestDTO;
+import dev.aprokhorenko.ticked_classifier.dto.TicketClasification;
 import dev.aprokhorenko.ticked_classifier.entities.Ticket;
 import dev.aprokhorenko.ticked_classifier.repositories.TicketRepository;
 import jakarta.transaction.Transactional;
@@ -14,9 +15,11 @@ import jakarta.transaction.Transactional;
 public class TicketServices {
 
     private TicketRepository tickedRepository;
+    private TicketClasificationServices ticketClasificationServices;
 
-    public TicketServices(TicketRepository tickedRepository) {
+    public TicketServices(TicketRepository tickedRepository, TicketClasificationServices ticketClasificationServices) {
         this.tickedRepository = tickedRepository;
+        this.ticketClasificationServices = ticketClasificationServices;
     }
 
     public Page<Ticket> getAllTickeds(Integer page, Integer sizePerPage) {
@@ -24,8 +27,14 @@ public class TicketServices {
         return tickedRepository.findAll(pageable);
     }
 
-    @Transactional 
+    @Transactional
     public void createTicked(CreateTicketRequestDTO request) {
-        
+        TicketClasification classification = ticketClasificationServices.classifyTicket(request.getTitle(),
+                request.getDescription());
+
+        Ticket ticket = new Ticket(request.getTitle(), request.getDescription(), classification.category(),
+                classification.urgency(), classification.feeling());
+
+        tickedRepository.save(ticket);
     }
 }
